a30419c09ff5b0a17b6042815b7cbcc72fda847d
Fernando Tessmann <nandotess85@gmail.com>
2017-06-08 20:50:41 -0300

    * Dev - LSX Sharing styles moved from LSX to LSX Sharing plugin

--- lsx-sharing.php
+++ lsx-sharing.php
@@ -3,7 +3,7 @@
  * Plugin Name: LSX Sharing
  * Plugin URI:  https://www.lsdev.biz/product/lsx-sharing/
  * Description: Sharing plugin for LSX Theme.
- * Version:     1.0.0
+ * Version:     1.0.1
  * Author:      LightSpeed
  * Author URI:  https://www.lsdev.biz/
  * License:     GPL3
@@ -20,7 +20,7 @@ if ( ! defined( 'WPINC' ) ) {
 define( 'LSX_SHARING_PATH', plugin_dir_path( __FILE__ ) );
 define( 'LSX_SHARING_CORE', __FILE__ );
 define( 'LSX_SHARING_URL',  plugin_dir_url( __FILE__ ) );
-define( 'LSX_SHARING_VER',  '1.0.0' );
+define( 'LSX_SHARING_VER',  '1.0.1' );
 
 /* ======================= The API Classes ========================= */
 
@@ -86,7 +86,7 @@ function lsx_sharing_api_admin_init() {
 
 	$api_array = array(
 		'product_id' => 'LSX Sharing',
-		'version'    => '1.0.0',
+		'version'    => '1.0.1',
 		'instance'   => $instance,
 		'email'      => $data['email'],
 		'api_key'    => $data['api_key'],
