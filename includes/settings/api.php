7c5d7f769fa9d472a8e07f8ab8bf7a3937e0fb71
Fernando Tessmann <nandotess85@gmail.com>
2017-09-22 09:04:27 -0500

    Travis-CI review

--- includes/settings/api.php
+++ includes/settings/api.php
@@ -1,10 +1,10 @@
 <div class="uix-field-wrapper">
-
 	<?php
-	$display_settings_page = false;
-	if ( class_exists( 'LSX_Currencies' ) ) {
-		$display_settings_page = true;
-	}
+		$display_settings_page = false;
+
+		if ( class_exists( 'LSX_Currencies' ) ) {
+			$display_settings_page = true;
+		}
 	?>
 
 	<ul class="ui-tab-nav">
@@ -14,10 +14,11 @@
 
 	<?php if ( false !== $display_settings_page ) { ?>
 		<div id="ui-settings" class="ui-tab active">
-			<p><?php esc_html_e('Please enter your user details (email address, API key, username, etc) below as required for the extensions that you have installed.','lsx-sharing' ); ?></p>
+			<p><?php esc_html_e( 'Please enter your user details (email address, API key, username, etc) below as required for the extensions that you have installed.', 'lsx-sharing' ); ?></p>
+
 			<table class="form-table" style="margin-top:-13px !important;">
 				<tbody>
-				<?php do_action( 'lsx_framework_api_tab_content', 'settings' ); ?>
+					<?php do_action( 'lsx_framework_api_tab_content', 'settings' ); ?>
 				</tbody>
 			</table>
 		</div>
@@ -26,18 +27,20 @@
 	<div id="ui-keys" class="ui-tab <?php if ( false === $display_settings_page ) { ?>active<?php } ?>">
 		<table class="form-table" style="margin-top:-13px !important;">
 			<tbody>
-			<?php
-			$lsx = admin_url('themes.php?page=lsx-welcome');
-			$message = sprintf( "Please enter the license and API key's for your add-ons below." );
-			$message .= sprintf( " Follow this <a href='%s' title='LSX add-ons'>link</a> to see what extensions are available for LSX.", $lsx );
-			?>
-			<p class="info"><?php echo wp_kses_post($message); ?></p>
-			<?php
-			$api_keys_content = false;
-			ob_start();
-			do_action( 'lsx_framework_api_tab_content', 'api' );
-			$api_keys_content = ob_end_clean();
-			?>
+				<?php
+					$lsx = admin_url( 'themes.php?page=lsx-welcome' );
+					$message = sprintf( "Please enter the license and API key's for your add-ons below." );
+					$message .= sprintf( " Follow this <a href='%s' title='LSX add-ons'>link</a> to see what extensions are available for LSX.", $lsx );
+				?>
+
+				<p class="info"><?php echo wp_kses_post( $message ); ?></p>
+
+				<?php
+					$api_keys_content = false;
+					ob_start();
+					do_action( 'lsx_framework_api_tab_content', 'api' );
+					$api_keys_content = ob_end_clean();
+				?>
 			</tbody>
 		</table>
 	</div>
