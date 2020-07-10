84b76fe0345ab053a12ed62afda9379027d43d07
Warwick Booth <warwick@lsdev.biz>
2020-07-10 13:53:53 +0200

    Updating the classes

--- classes/class-sharing.php
+++ classes/class-sharing.php
@@ -4,21 +4,79 @@
  *
  * @package lsx-sharing
  */
+namespace lsx\sharing;
 
-if ( ! class_exists( 'LSX_Sharing' ) ) {
+/**
+ * LSX Sharing class.
+ *
+ * @package lsx-sharing
+ */
+class Sharing {
 
 	/**
-	 * LSX Sharing class.
+	 * Holds class instance
 	 *
-	 * @package lsx-sharing
+	 * @since 1.0.0
+	 *
+	 * @var      object \lsx\search\Sharing()
 	 */
-	class LSX_Sharing {
+	protected static $instance = null;
 
-		/**
-		 * Constructor.
-		 */
-		public function __construct() {}
+	/**
+	 * Holds class instance
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var      object \lsx\search\classes\Admin()
+	 */
+	public $admin = false;
 
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		$this->load_vendors();
+		$this->load_classes();
+	}
+
+	/**
+	 * Return an instance of this class.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return    object \lsx\member_directory\search\Admin()    A single instance of this class.
+	 */
+	public static function get_instance() {
+		// If the single instance hasn't been set, set it now.
+		if ( null === self::$instance ) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * Loads the plugin functions.
+	 */
+	private function load_vendors() {
+		// Configure custom fields.
+		if ( ! class_exists( 'CMB2' ) ) {
+			require_once LSX_SHARING_PATH . 'vendor/CMB2/init.php';
+		}
+	}
+	/**
+	 * Loads the plugin functions.
+	 */
+	private function load_classes() {
+		require_once LSX_SHARING_PATH . '/classes/class-admin.php';
+		require_once LSX_SHARING_PATH . '/classes/class-lsx-sharing-frontend.php';
+		require_once LSX_SHARING_PATH . '/classes/class-lsx-sharing-button.php';
+
+		global $lsx_sharing_admin;
+		$this->admin = \lsx\sharing\classes\Admin::get_instance();
+		$lsx_sharing_admin;
+
+		global $lsx_sharing;
+		$lsx_sharing = new \LSX_Sharing_Frontend();
 	}
 
 }
--- lsx-sharing.php
+++ lsx-sharing.php
@@ -3,7 +3,7 @@
  * Plugin Name: LSX Sharing
  * Plugin URI:  https://www.lsdev.biz/product/lsx-sharing/
  * Description: The LSX Sharing extension add social share icons for LSX Theme.
- * Version:     1.1.3
+ * Version:     1.2.0
  * Author:      LightSpeed
  * Author URI:  https://www.lsdev.biz/
  * License:     GPL3
@@ -20,17 +20,17 @@ if ( ! defined( 'WPINC' ) ) {
 define( 'LSX_SHARING_PATH', plugin_dir_path( __FILE__ ) );
 define( 'LSX_SHARING_CORE', __FILE__ );
 define( 'LSX_SHARING_URL', plugin_dir_url( __FILE__ ) );
-define( 'LSX_SHARING_VER', '1.1.3' );
+define( 'LSX_SHARING_VER', '1.2.0' );
 
 /* ======================= Below is the Plugin Class init ========================= */
-
-require_once LSX_SHARING_PATH . '/classes/class-lsx-sharing.php';
-require_once LSX_SHARING_PATH . '/classes/class-admin.php';
-require_once LSX_SHARING_PATH . '/classes/class-lsx-sharing-frontend.php';
-require_once LSX_SHARING_PATH . '/classes/class-lsx-sharing-button.php';
-
-global $lsx_sharing_admin;
-$lsx_sharing_admin = \lsx\sharing\classes\Admin::get_instance();
-
-global $lsx_sharing;
-$lsx_sharing = new LSX_Sharing_Frontend();
+require_once LSX_SHARING_PATH . '/classes/class-sharing.php';
+/**
+ * Returns and instance of the LSX Sharing plugin.
+ *
+ * @return object \lsx\sharing\Sharing();
+ */
+function lsx_sharing() {
+	$lsx_sharing = \lsx\sharing\Sharing::get_instance();
+	return $lsx_sharing;
+}
+lsx_sharing();
